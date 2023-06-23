66009608aff9296640635c95c3ba1e9b09102c29
Kanat9494 <kkanat94@gmail.com>
2023-06-23 22:51:21 +0600

    Add project files.

---
+++ ProductsFC/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductsFC;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ ProductsFC/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ProductsFC;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
