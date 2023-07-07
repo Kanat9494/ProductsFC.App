4cbab42b86ce66748d1f0b9a094298fd4143e302
kana2022 <kkanat94@gmail.com>
2023-07-07 09:57:39 +0600

    2

--- ProductsFC/Models/Product.cs
+++ ProductsFC/Models/Product.cs
@@ -6,8 +6,8 @@ public class Product
     public int ProductId { get; set; }
     public string Name { get; set; }
     public string TrackCode { get; set; }
-    public double? Price { get; set; }
-    public double? Weight { get; set; }
+    public double? Price { get; set; } = null;
+    public double? Weight { get; set; } = null;
     public string OrderDate { get; set; }
     public byte IsDelivered { get; set; }
     public string ImageUrl { get; set; }
