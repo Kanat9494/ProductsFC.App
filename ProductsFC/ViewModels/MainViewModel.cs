6a8dbe27dbce40c245590aa544143c42d559f3ac
kana2022 <kkanat94@gmail.com>
2023-07-27 17:07:21 +0600

    1

--- ProductsFC/ViewModels/MainViewModel.cs
+++ ProductsFC/ViewModels/MainViewModel.cs
@@ -9,6 +9,14 @@ public class MainViewModel : BaseViewModel
         Products = new ObservableCollection<Product>();
         NewProductCommand = new Command(async () => await OnNewProduct());
         ProductDetailsCommand = new Command<int>(async (productId) => await OnProductDetails(productId));
+        DeleteItemCommand = new Command<Product>(async (product) =>
+        {
+            await OnDeleteItem(product);
+        });
+        ItemDeliveredCommand = new Command<Product>(async (product) =>
+        {
+            await OnItemDelivered(product);
+        });
         ProductsListToSearch = new List<Product>();
         RefreshProductsCommand = new Command(() =>
         {
@@ -39,7 +47,12 @@ public class MainViewModel : BaseViewModel
     public ICommand NewProductCommand { get; }
     public ICommand ProductDetailsCommand { get; }
     public ICommand RefreshProductsCommand { get; }
+    public ICommand DeleteItemCommand { get; }
+    public ICommand ItemDeliveredCommand { get; }
 
+
+
+    #region Properties
     private string _searchText;
     public string SearchText
     {
@@ -58,6 +71,10 @@ public class MainViewModel : BaseViewModel
         get => _productsCount;
         set => SetProperty(ref _productsCount, value);
     }
+
+
+
+    #endregion
 
     private async Task LoadProducts()
     {
@@ -102,5 +119,32 @@ public class MainViewModel : BaseViewModel
             for (int i = 0; i < products.Count; i++)
                 Products.Add(products[i]);
         }
+    }
+
+    private async Task OnDeleteItem(Product product)
+    {
+        var result = await _goodsDbService.DeleteItemAsync(product);
+
+        if (result == 0)
+        {
+            await Shell.Current.DisplayAlert("Ошибка", "Произошла непредвиденная ошибка, обновите экран и попробуйте удалить ещё раз", "Ок");
+            return;
+        }
+
+        Products.Remove(product);
+    }
+
+    private async Task OnItemDelivered(Product product)
+    {
+        product.IsDelivered = 1;
+        var result = await _goodsDbService.UpdateItemAsync(product);
+
+        if (result == 0)
+        {
+            await Shell.Current.DisplayAlert("Ошибка", "Произошла непредвиденная ошибка, обновите экран и попробуйте сделать доставлено ещё раз", "Ок");
+            return;
+        }
+
+        Products.Remove(product);
     }
 }
